53ebcfd9addd89c8097d5d94a53dc201eb751776
PADELEGANECOPRINT <padelegankknt@gmail.com>
2024-10-07 22:51:26 +0700

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+document.querySelectorAll('.btnDetail').forEach(item => {
+    item.addEventListener('click',(e) => {
+        let parent = e.target.parentNode.parentNode;
+
+        let gambar = parent.querySelector('.card-img-top').src;
+        let harga = parent.querySelector('.harga').innerHTML;
+        let prodak = parent.querySelector('.card-text').innerHTML;
+        let deskripsi = parent.querySelector('.deskripsi') 
+    ? parent.querySelector('.deskripsi').innerHTML 
+    : '<i> tidak ada informasi yang tersedia</i>';
+
+    let tombolModal = document.querySelector('.btnModal');
+    tombolModal.click();
+
+    document.querySelector('.modalTitle').innerHTML = prodak;
+    let image = document.createElement('img');
+    image.src = gambar; 
+    image.classList.add('w-100');
+    document.querySelector('.modalImage').innerHTML = '';
+    document.querySelector('.modalImage').appendChild(image);
+    document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
+    document.querySelector('.modalHarga').innerHTML = harga;
+
+    const nohp = '6283832882778'; 
+    let pesan = 'https://api.whatsapp.com/send?phone=$(nohp)&text=Hallo Kak, saya ingin pesan produk ini ${gambar}';
+
+    document.querySelector('.btnBeli').href = pesan;
+    });
+});
